c0b6f0f0fa90acef3a2e2313035338bf3b42e0bc
Atiskova Anastasia <mailbox.nk@mail.ru>
2022-10-16 22:05:20 +0300

    Добавили класс Main, Person, PersonComparator

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+        List<Person> listPeople = new ArrayList<>();
+        listPeople.add(new Person("Анна", "Мырсакат Уллу", 30));
+        listPeople.add(new Person("Адриан", "Танги Мио Ти", 25));
+        listPeople.add(new Person("Лоди", "Юдин Мыс", 28));
+        listPeople.add(new Person("Мадин", "Фролов", 28));
+        System.out.println(listPeople);
+
+       // Collections.sort(listPeople);
+
+        Collections.sort(listPeople, new PersonComparator(5));
+        System.out.println(listPeople);
+    }
+
+}
---
+++ src/PersonComparator.java
@@ -0,0 +1,18 @@
+import java.util.Comparator;
+
+public class PersonComparator implements Comparator<Person> {
+
+    public PersonComparator(int surnameLen) {
+    }
+
+    @Override
+    public int compare(Person p1, Person p2) {
+        int len1 = p1.getSurname().split(" ").length;
+        int len2 = p2.getSurname().split(" ").length;
+        if (len1 != len2) {
+            return Integer.compare(len1, len2); //это тоже самое что len1 - len2
+        }
+        return Integer.compare(p1.getAge(), p2.getAge());
+    }
+
+}
